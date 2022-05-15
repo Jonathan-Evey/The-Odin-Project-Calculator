d1b52cc0b358ed25063dfc54c3c02bdbb990daa7
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-05-15 08:48:30 -0700

    add basic math functions: add, subtract, multiply and divide

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+/*---Basic function that returns sums two numbers---*/
+function add(num1, num2) {
+    return num1 + num2;
+}
+/*---Basic function that returns sums of a number minus a number---*/
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+/*---Basic function that returns product of two numbers---*/
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+/*---Basic function that product of a number divided by a number---*/
+function divide(num1, num2) {
+    return num1 / num2;
+}
