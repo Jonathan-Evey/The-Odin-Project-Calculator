c66af1c11bc63c75a90708d0a3017b1261522d6c
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-05-15 12:48:02 -0700

    add equation operator display function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const inputDisplay = document.getElementById('user-input-display');
 const storedInput = document.getElementById('stored-user-input');
+const equationOperatorDisplay = document.getElementById('equation-operator-display')
 const numberButtons = document.querySelectorAll('.number');
+const equationOperatorButtons = document.querySelectorAll('.equation-operator');
 
-console.log(inputDisplay);
-console.log(storedInput);
-console.log(numberButtons);
+console.log(equationOperatorButtons);
 
-
-function btnListener() {
+/*---check for user interaction with number buttons/updates display on click event---*/
+function numberButtonListener() {
     numberButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
         updateDisplay(e);
@@ -15,14 +15,51 @@ function btnListener() {
     })
 };
 
-/*---updates display to add number that was clicked. If it is the first one will update the 0 to new number---*/
+/*---updates display to add number that was clicked. If it is the first one will update the starting 0 to first user input number---*/
 function updateDisplay(e) {
     if (display === 0) {
         display = e.target.innerText;
+        currentInputDisplay();
     } else {
     display = display + e.target.innerText;
     currentInputDisplay();
     }
+}
+
+function equationOperatorButtonListener() {
+    equationOperatorButtons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            updateEquationOperatorDisplay(e);
+            updateStoredInputDisplay();
+            console.log(operatorDisplay);
+        })
+    })
+};
+
+function updateEquationOperatorDisplay(e) {
+    if (e.target.innerText == '+') {
+        operatorDisplay = '+'; 
+    }
+    if (e.target.innerText == '-') {
+        operatorDisplay = '-'; 
+    }
+    if (e.target.innerText == 'x') {
+        operatorDisplay = '*'; 
+    }
+    if (e.target.innerText == '/') {
+        operatorDisplay = '/'; 
+    }
+}
+
+function updateStoredInputDisplay() {
+    if (storedDisplay === null) {
+        storedDisplay = display;
+        display = 0;
+        currentEquationOperatorDisplay();
+        currentStoredInputDisplay();
+        currentInputDisplay();
+    }
+
 }
 
 /*---Basic function to evaluate what math function to use---*/
@@ -68,6 +105,13 @@ function currentStoredInputDisplay() {
     storedInput.innerText = storedDisplay;
 }
 
-btnListener();
+let operatorDisplay = null;
+function currentEquationOperatorDisplay() {
+    equationOperatorDisplay.innerText = operatorDisplay;
+}
+
+numberButtonListener();
+equationOperatorButtonListener();
 currentInputDisplay();
 currentStoredInputDisplay();
+currentEquationOperatorDisplay();
